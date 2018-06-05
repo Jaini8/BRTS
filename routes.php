1ea60bd3aa1d6027946c7aaa3c2cf236d17e2f3c
vnd7 <39335185+vnd7@users.noreply.github.com>
2018-06-05 15:03:46 +0530

    Add files via upload

--- routes.php
+++ routes.php
@@ -6,20 +6,35 @@ $r1d=array_reverse($r1u);
 $r2u=array("Vasna","Anjali","Chandranagar","Khodiyarnagar","Danilimda C.Rasta","Danilimda Road","Chhipa","Chandola Lake","BRTS Workshop","Kashiram","Narol","Mukesh Ind.","Isanpur","Ghodasar","Jashodanagar","Express Way","C.T.M.","Hateshwar","Purvadeep","Jogeshwari Soc.","Rabari Colony","Ramrajya Soc.","Rameshwar Park","Geeta Gauri","Soni Ni Chali","Viratnagar","Bapunagar","Leelanagar","Thakkarnagar","Hirawadi","Vijay Park","Krishnanagar","Dhanush Dhari","Naroda","Bethak","Naroda Gam");
 $r2d=array_reverse($r2u);
 //rto to naroda
-$r3u=array("RTO Circle","Ranip Cross Road","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Sola Cross Road","Valinath Chowk","Memnagar","University","Andhajan Mandal","Himatlal Park","Shivaranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar","Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","Danilimda Road","Chhipa","Chandola Lake","BRTS Workshop","Kashiram","Narol","Mukesh Ind.","Isanpur","Ghodasar","Jashodanagar","Express Way","C.T.M.","Hateshwar","Purvadeep","Jogeshwari Soc.","Rabari Colony","Ramrajya Soc.","Rameshwar Park","Geeta Gauri","Soni Ni Chali","Viratnagar","Bapunagar","Leelanagar","Thakkarnagar","Hirawadi","Vijay Park","Krishnanagar","Dhanush Dhari","Naroda");
+$r3u=array("RTO Circle","Ranip Cross Road","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Sola Cross Road","Valinath Chowk","Memnagar","University","Andhajan Mandal","Himatlal Park","Shivaranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar","Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","Danilimda Road","Chhipa","Chandola Lake","BRTS Workshop","Kashiram","Narol","Mukesh Ind.","Isanpur","Ghodasar","Jashodanagar","Express Way","C.T.M","Hateshwar","Purvadeep","Jogeshwari Soc.","Rabari Colony","Ramrajya Soc.","Rameshwar Park","Geeta Gauri","Soni Ni Chali","Viratnagar","Bapunagar","Leelanagar","Thakkarnagar","Hirawadi","Vijay Park","Krishnanagar","Dhanush Dhari","Naroda");
 $r3d=array_reverse($r3u);
 //rto to delhi darwaja
 $r4u=array("RTO Circle","Ranip Cross Road","NR Patel","Ramapir no teka","Junavadaj","Gurudwara","Hanumanpura","Sarkari Litho Press","Dilhi Darwaja","Delhi Darwaja");
 $r4d=array_reverse($r4u);
 //ghuma to maninagar
-$r5u=array("Ghuma Gam","Bopal","Bopal Approach","Ambali Gam","Swagat Bunglows","Jayantilal Park","Ashok Vatika","Antarix Colony","Iskcon Mandir","ISKCON Cross Road","Ramdev Nagar","ISRO","Star Bazaar","Jodhpur Char Rasta","Shivranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar","Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","V. Mandir","S.N. College","Kankriya T.Ex.","Kankaria Lake","Rambaug","Maninagar C.Road","Maninagar R.S");
+$r5u=array("Ghuma Gam","Bopal Gam","Sterling City","Bhavya Park","Samarpan Bunglows","Bopal Approach","Ambali Gam","Swagat Bunglow","Jayantilal Park","Ashok Vatika","Antarix Colony","Iskcon Mandir","ISKCON Cross Road","Ramdev Nagar","ISRO","Star Bazaar","Jodhpur Char Rasta","Shivranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar","Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","V. Mandir","S.N. College","Kankriya T.Ex.","Kankaria Lake","Rambaug","Maninagar C.Road","Maninagar R.S");
 $r5d=array_reverse($r5u);
 //odhav to sp ring road
 $r6u=array("AJIT Mill","Soni Ni Chali","Grid Station","Odhav Fire Station","Vallabh Nagar","Chhotalal ni Chali","Morlidhar Society","Odhav Talav (Gam)","S P Ring Road Approach");
 $r6d=array_reverse($r6u);
 //delhi darwaja to science city approach
-$r7u=array("Delhi Darwaja","Dilhi Darwaja","Sarkari Litho Press","Hanumanpura","Gurudwara","Junavadaj","Ramapir no teka","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Paras nagar","Parshwanath Jain mandir","BhuyangDev","Sattadhar char Rasta","Sola Bridge","science city approach");
+$r7u=array("Delhi Darwaja","Dilhi Darwaja","Sarkari Litho Press","Hanumanpura","Gurudwara","Junavadaj","Ramapir no teka","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Paras nagar","Parshwanath Jain mandir","BhuyangDev","Sattadhar char Rasta","Sola Bridge","science city appraoch");
 $r7d=array_reverse($r7u);
+//chandkheda to maninagar
+$r8u=array("Zundal Circle","Sarthi Bunglows","Chandkheda Gam","Shiv Shaktinagar","Jantanagar","ONGC Avani Bhavan","Vishvkarma Engg College","Visat Junction","Motera Cross road","Sabarmati Police Station","Municipal Swimming Pool","Rathi Apartment","Power House","RTO Circle","Ranip Cross Road","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Sola Cross Road","Valinath Chowk","Memnagar","University","Andhajan Mandal","Himatlal Park","Shivaranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar","Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","V. Mandir","S.N. College","Kankriya T.Ex.","Kankaria Lake","Rambaug","Maninagar C.Road","Maninagar R.S");
+$r8d=array_reverse($r8u);
+//rto to commerce
+$r9u=array("RTO Circle","Ranip Cross Road","NR Patel","Ramapir no teka","Junavadaj","Gurudwara","Hanumanpura","Sarkari Litho Press","Dilhi Darwaja","Delhi Darwaja","Prem Darwaja","Kalupur Ghee Bazar","Kalupur Railway Station","Sarangpur Darwaja","Karnmukteshwar Mahadev","Raipur Darwaja","Aastodia Darwaja","Aastodia Chakla","AMC office","Raikhad Char Rasta","Lokmanya Tilak Bag","Townhall","M J Library","Law garden","Vasundha Society","LD engineering clg","Commerce Six Road");
+$r9d=array_reverse($r9u);
+//vasna to kalupur
+$r10u=array("Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","V. Mandir","S.N. College","Raipur Darwaja","Karnmukteshwar Mahadev","Sarangpur Darwaja","Kalupur Railway Station");
+$r10d=array_reverse($r10u);
+//iskcon to naroda
+$r11u=array("ISKCON Cross Road","Ramdev Nagar","ISRO","Star Bazaar","Jodhpur Char Rasta","Shivranjani","Jhansi-ki-rani","Nehrunagar","L colony","Panjara pole","Gulbai Tekra","LD engineering clg","Vasundha Society","Law garden","M J Library","Townhall","Lokmanya Tilak Bag","Raikhad Char Rasta","AMC office","Aastodia Chakla","Aastodia Darwaja","Raipur Darwaja","Karnmukteshwar Mahadev","Sarangpur Darwaja","Kalupur Railway Station","G.C.S. Hospital","Arvind Mill","Geening Press","Ashok Mill","Naroda Fruit Market","Memco Cross Road","Municipal North Zone Office","G.D.School","Saipur Tower","Naroda Gam","Bethak","Naroda");
+$r11d=array_reverse($r11u);
+//anjali to anjali
+$r12u=array("Anjali","Chandranagar","Khodiyarnagar","Danilimda C. Rasta","V. Mandir","S.N. College","Raipur Darwaja","Karnmukteshwar Mahadev","Sarangpur Darwaja","Kalupur Railway Station","Delhi Darwaja","Dilhi Darwaja","Sarkari Litho Press","Hanumanpura","Gurudwara","Junavadaj","Ramapir no teka","Bhavsar Hostel","Akhbarnagar","Pragatinagar","Shastrinagar","Jaimangal","Sola Cross Road","Valinath Chowk","Memnagar","University","Andhajan Mandal","Himatlal Park","Shivaranjani","Jhansi-ki-rani","Nehrunagar","Manekbaug","Dharnidhar");
+$r12d=array_reverse($r12u);
 $source=$_POST['ca'];
 $dest=$_POST['cb'];
 $n1=sizeof($r1u);
@@ -29,6 +44,13 @@ $n4=sizeof($r4u);
 $n5=sizeof($r5u);
 $n6=sizeof($r6u);
 $n7=sizeof($r7u);
+$n8=sizeof($r8u);
+$n9=sizeof($r9u);
+$n10=sizeof($r10u);
+$n11=sizeof($r11u);
+$n12=sizeof($r12u);
+if($source!=$dest)
+{
 //route 1
 for($i=0;$i<$n1;$i++)
 {
@@ -38,10 +60,10 @@ for($i=0;$i<$n1;$i++)
 		{
 			if($dest==$r1u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r1u[$k] ."<br>" ;
+					echo "-> ". $r1u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -55,10 +77,10 @@ for($i=0;$i<$n1;$i++)
 		{
 			if($dest==$r1d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r1d[$k] ."<br>" ;
+					echo "-> ". $r1d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -73,10 +95,10 @@ for($i=0;$i<$n2;$i++)
 		{
 			if($dest==$r2u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r2u[$k] ."<br>" ;
+					echo "-> ". $r2u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -90,10 +112,10 @@ for($i=0;$i<$n2;$i++)
 		{
 			if($dest==$r2d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r2d[$k] ."<br>" ;
+					echo "-> ". $r2d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -108,10 +130,10 @@ for($i=0;$i<$n3;$i++)
 		{
 			if($dest==$r3u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r3u[$k] ."<br>" ;
+					echo "-> ". $r3u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -125,10 +147,10 @@ for($i=0;$i<$n3;$i++)
 		{
 			if($dest==$r3d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r3d[$k] ."<br>" ;
+					echo "-> ". $r3d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -143,10 +165,10 @@ for($i=0;$i<$n4;$i++)
 		{
 			if($dest==$r4u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r4u[$k] ."<br>" ;
+					echo "-> ". $r4u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -160,10 +182,10 @@ for($i=0;$i<$n4;$i++)
 		{
 			if($dest==$r4d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r4d[$k] ."<br>" ;
+					echo "-> ". $r4d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -178,10 +200,10 @@ for($i=0;$i<$n5;$i++)
 		{
 			if($dest==$r5u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r5u[$k] ."<br>" ;
+					echo "-> ". $r5u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -195,10 +217,10 @@ for($i=0;$i<$n5;$i++)
 		{
 			if($dest==$r5d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r5d[$k] ."<br>" ;
+					echo "-> ". $r5d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -213,10 +235,10 @@ for($i=0;$i<$n6;$i++)
 		{
 			if($dest==$r6u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r6u[$k] ."<br>" ;
+					echo "-> ". $r6u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -230,10 +252,10 @@ for($i=0;$i<$n6;$i++)
 		{
 			if($dest==$r6d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r6d[$k] ."<br>" ;
+					echo "-> ". $r6d[$k] ."<br>" ;
 				}
 			}
 		}
@@ -248,10 +270,10 @@ for($i=0;$i<$n7;$i++)
 		{
 			if($dest==$r7u[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r7u[$k] ."<br>" ;
+					echo "-> ". $r7u[$k] ."<br>" ;
 				}
 			}
 		}
@@ -261,19 +283,200 @@ for($i=0;$i<$n7;$i++)
 {
 	if($source==$r7d[$i])
 	{
-		for($j=$i;$j<$n7;$j++)
+		for($j=$i;$j<$n1;$j++)
 		{
 			if($dest==$r7d[$j])
 			{	
-				echo "Your Bus Route is : <br>";
+				echo "<b> Your Bus Route is </b>: <br>";
 				for($k=$i;$k<=$j;$k++)
 				{
-					echo $r7d[$k] ."<br>" ;
+					echo "-> ". $r7d[$k] ."<br>" ;
 				}
 			}
 		}
 	}
 }
-				
+//route 8
+for($i=0;$i<$n8;$i++)
+{
+	if($source==$r8u[$i])
+	{
+		for($j=$i;$j<$n8;$j++)
+		{
+			if($dest==$r8u[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r8u[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+for($i=0;$i<$n8;$i++)
+{
+	if($source==$r8d[$i])
+	{
+		for($j=$i;$j<$n8;$j++)
+		{
+			if($dest==$r8d[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r8d[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+//route 9
+for($i=0;$i<$n9;$i++)
+{
+	if($source==$r9u[$i])
+	{
+		for($j=$i;$j<$n9;$j++)
+		{
+			if($dest==$r9u[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r9u[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+for($i=0;$i<$n9;$i++)
+{
+	if($source==$r9d[$i])
+	{
+		for($j=$i;$j<$n9;$j++)
+		{
+			if($dest==$r9d[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r9d[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+//route 10
+for($i=0;$i<$n10;$i++)
+{
+	if($source==$r10u[$i])
+	{
+		for($j=$i;$j<$n10;$j++)
+		{
+			if($dest==$r10u[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r10u[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+for($i=0;$i<$n10;$i++)
+{
+	if($source==$r10d[$i])
+	{
+		for($j=$i;$j<$n10;$j++)
+		{
+			if($dest==$r10d[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r10d[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+//route 11
+for($i=0;$i<$n11;$i++)
+{
+	if($source==$r11u[$i])
+	{
+		for($j=$i;$j<$n11;$j++)
+		{
+			if($dest==$r11u[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r11u[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+for($i=0;$i<$n11;$i++)
+{
+	if($source==$r11d[$i])
+	{
+		for($j=$i;$j<$n11;$j++)
+		{
+			if($dest==$r11d[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r11d[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+//route 12
+for($i=0;$i<$n12;$i++)
+{
+	if($source==$r12u[$i])
+	{
+		for($j=$i;$j<$n12;$j++)
+		{
+			if($dest==$r12u[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r12u[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+for($i=0;$i<$n12;$i++)
+{
+	if($source==$r12d[$i])
+	{
+		for($j=$i;$j<$n12;$j++)
+		{
+			if($dest==$r12d[$j])
+			{	
+				echo "<b> Your Bus Route is </b>: <br>";
+				for($k=$i;$k<=$j;$k++)
+				{
+					echo "-> ". $r12d[$k] ."<br>" ;
+				}
+			}
+		}
+	}
+}
+}
+else
+{
+echo "<script> alert('Same Source and Destination'); </script>";
+include("rtick.php");
+}
+
 ?>
 
